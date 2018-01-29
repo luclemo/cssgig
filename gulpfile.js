de9f29305795db47ac48fde02ce68ca178c8deea
Lucas <ll@lucaslemonnier.com>
2018-01-29 13:12:20 -0500

    CleanCSS arguments to remove duplicate rules

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,28 @@ gulp.task("styles", function() {
         browsers: ["last 3 versions"]
       })
     )
-    .pipe(cleanCSS())
+    .pipe(
+      cleanCSS({
+        level: {
+          2: {
+            mergeAdjacentRules: true, // controls adjacent rules merging; defaults to true
+            mergeIntoShorthands: true, // controls merging properties into shorthands; defaults to true
+            mergeMedia: true, // controls `@media` merging; defaults to true
+            mergeNonAdjacentRules: true, // controls non-adjacent rule merging; defaults to true
+            mergeSemantically: false, // controls semantic merging; defaults to false
+            overrideProperties: true, // controls property overriding based on understandability; defaults to true
+            removeEmpty: true, // controls removing empty rules and nested blocks; defaults to `true`
+            reduceNonAdjacentRules: true, // controls non-adjacent rule reducing; defaults to true
+            removeDuplicateFontRules: true, // controls duplicate `@font-face` removing; defaults to true
+            removeDuplicateMediaBlocks: true, // controls duplicate `@media` removing; defaults to true
+            removeDuplicateRules: true, // controls duplicate rules removing; defaults to true
+            removeUnusedAtRules: false, // controls unused at rule removing; defaults to false (available since 4.1.0)
+            restructureRules: false, // controls rule restructuring; defaults to false
+            skipProperties: [] // controls which properties won't be optimized, defaults to `[]` which means all will be optimized (since 4.1.0)
+          }
+        }
+      })
+    )
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("_build/css"))
     .pipe(reload({ stream: true }));
